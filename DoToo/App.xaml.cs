6032601cb606274597de84df1496353ed1ac22f7
derrick235 <44071958+derrick235@users.noreply.github.com>
2021-05-16 21:54:44 -0400

    Add files via upload

---
+++ DoToo/App.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using MedicineTracker.Views;
+
+namespace MedicineTracker
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+
+            InitializeComponent();
+
+            MainPage = new NavigationPage(Resolver.Resolve<MainView>());
+            // MainPage = new MainPage();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ DoToo/Bootstrapper.cs
@@ -0,0 +1,40 @@
+﻿using Autofac;
+using System.Linq;
+using System.Reflection;
+using Xamarin.Forms;
+using MedicineTracker.Views;
+using MedicineTracker.Repositories;
+using MedicineTracker.ViewModels;
+
+namespace MedicineTracker
+{
+    public abstract class Bootstrapper
+    {
+        protected ContainerBuilder ContainerBuilder { get; private set; }
+
+        public Bootstrapper()
+        {
+            Initialize();
+            FinishInitialization();
+        }
+
+        protected virtual void Initialize()
+        {
+            var currentAssembly = Assembly.GetExecutingAssembly();
+            ContainerBuilder = new ContainerBuilder();
+
+            foreach (var type in currentAssembly.DefinedTypes
+                      .Where(e => e.IsSubclassOf(typeof(Page)) || e.IsSubclassOf(typeof(ViewModel))))
+            {
+                ContainerBuilder.RegisterType(type.AsType());
+            }
+            ContainerBuilder.RegisterType<TodoItemRepository>().SingleInstance();
+        }
+
+        private void FinishInitialization()
+        {
+            var container = ContainerBuilder.Build();
+            Resolver.Initialize(container);
+        }
+    }
+}
