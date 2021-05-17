079b972e526d526bdf75a16c71529af0e8b5cffc
derrick235 <44071958+derrick235@users.noreply.github.com>
2021-05-16 22:32:29 -0400

    Add files via upload

---
+++ MedicineTracker.UWP/Bootstrapper.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MedicineTracker.UWP
+{
+    public class Bootstrapper : MedicineTracker.Bootstrapper
+    {
+        public static void Init()
+        {
+            var instance = new Bootstrapper();
+        }
+    }
+}
